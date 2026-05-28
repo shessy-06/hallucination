0a34b503fe7d0e1554e8b562ac4b15feb2d472d0
shessymol <shessymol7025@gmail.com>
2026-05-28 22:15:12 +0530

    deploy setup

--- hallucination/settings.py
+++ hallucination/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-ahsi&o=4s8vlp=mh3l@4lb8xk_fd8w57t##$@07$2zd)8rs$9w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'hallucination.urls'
 
 TEMPLATES = [
@@ -150,3 +151,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'thehallucinationfilms@gmail.com'
 
 EMAIL_HOST_PASSWORD = 'lnnj rkhq bqds qjrk'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
